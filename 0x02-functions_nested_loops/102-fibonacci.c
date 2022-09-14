6b795ddf5fa821cb2af96c16a31be3b7c3401fe6
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-13 20:03:15 -0700

    fix: invalid uint syntax

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,12 @@
 int main(void)
 {
 	printf("1, 2, ");
-	long x=1, y=2;
+	unsigned int x=1;
+        unsigned int y=2;
 	for (int i=2;i<50;i++)
 	{
-		long z = x+y;
-		printf("%i, ", z);
+		unsigned z = x+y;
+		printf("%u, ", z);
 		x=y;	y=z;
 	}
 
