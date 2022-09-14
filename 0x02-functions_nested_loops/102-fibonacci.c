410e2401786f415629236af6c780ca489087591e
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-13 19:52:26 -0700

    fix: file names

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,16 @@
  * Return: number of digits
  */
 
-void main (void)
+int main (void)
 {
 	printf("1, 2, ");
-	for (unsigned int i=2,x=1,y=2;i<50;i++)
+	unsigned int x=1, y=2;
+	for (int i=2;i<50;i++)
 	{
 		unsigned int z = x+y;
-		printf("%i", z);
+		printf("%i, ", z);
 		x=y;	y=z;
 	}
+	printf('\n');
+	return 0;
 }
