e0d8ed09d55ffb002cdfbea7049fe9d54419c468
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-15 16:54:24 -0700

    fix: re code

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - prints numbers
+ * Return:0
  */
-int main(void)
-{
-	print_numbers();
 
-	return (0);
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+
+	}
+	_putchar('\n');
+
 }
