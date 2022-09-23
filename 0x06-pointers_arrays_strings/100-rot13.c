5080238f33149dfed2072b419efbb11703abecc7
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-22 18:57:44 -0700

    fix: recode

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,44 +1,29 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: returns the encoded string
-*/
-
-char *rot13(char *str)
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
+ */
+char *rot13(char *s)
 {
-	int index1, index2;
+	int i;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-											 'G', 'H', 'I', 'J', 'K', 'L',
-											 'M', 'N', 'O', 'P', 'Q', 'R',
-											 'S', 'T', 'U', 'V', 'W', 'X',
-											 'Y', 'Z', 'a', 'b', 'c', 'd',
-											 'e', 'f', 'g', 'h', 'i', 'j',
-											 'k', 'l', 'm', 'n', 'o', 'p',
-											 'q', 'r', 's', 't', 'u', 'v',
-											 'w', 'x', 'y', 'z'};
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
 
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-											 'T', 'U', 'V', 'W', 'X', 'Y',
-											 'Z', 'A', 'B', 'C', 'D', 'E',
-											 'F', 'G', 'H', 'I', 'J', 'K',
-											 'L', 'M', 'n', 'o', 'p', 'q',
-											 'r', 's', 't', 'u', 'v', 'w',
-											 'x', 'y', 'z', 'a', 'b', 'c',
-											 'd', 'e', 'f', 'g', 'h', 'i',
-											 'j', 'k', 'l', 'm'};
-	while (str[++index1])
+	while (*s)
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (i = 0; i <= 52; i++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (*s == rot13[i])
 			{
-				str[index1] = rot13key[index2];
+				*s = ROT13[i];
 				break;
 			}
 		}
+		s++;
 	}
-	return (str);
+	return (ptr);
 }
