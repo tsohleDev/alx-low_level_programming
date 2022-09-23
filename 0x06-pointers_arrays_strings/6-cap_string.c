6761007692207b553041786d079ff22bc987f462
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-22 18:43:10 -0700

    fix: dereference

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,64 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
-char *cap_string(char *str)
+ * isLower - Determines whether ascii is lowercase
+ *
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isLower(char c)
 {
-	int index = 0;
+	return (c >= 97 && c <= 122);
+}
 
-	while (str[index] != '\0')
+/**
+ * isDelimiter - Determines whether ascii is a delimiter
+ *
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: Input string
+ *
+ * Return: string with capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
 	{
-		if ((str[index] >= 'a' && str[index] <= 'z'))
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			*str[index] -= 32;
+			*s -= 32;
+			foundDelimit = 0;
 		}
-
-		index++;
-
+		else
+			foundDelimit = 0;
+		s++;
 	}
-	return (str);
+	return (ptr);
 }
