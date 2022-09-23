374c47badb263e5867063aab04e2d3cdfe1a3eec
tsohleDev <mokhemisitsohle@gmail.com>
2022-09-22 18:27:30 -0700

    fix: loop

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,15 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[++index])
+	while (str[index] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
+		if ((str[index] >= 'a' && str[index] <= 'z'))
+		{
 			str[index] -= 32;
+		}
+
+		index++;
+
 	}
 	return (str);
 }
