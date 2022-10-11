ece3625a390ac881cd61a3b30a6b7e62d27bbae0
tsohleDev <mokhemisitsohle@gmail.com>
2022-10-10 20:02:50 -0700

    Fix null cases
    instead
    of nul must

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,22 +8,24 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL )
-return; 
+	if (d == NULL )
+		return; 
 
-if ((*d).name == NULL)
-printf("nil\n");
-else
-printf("Name: %s\n", d->name);
+	if ((*d).name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-if ((*d).age < 0)
-printf("nil\n");
-else
-printf("Age: %f\n", d->age);
 
-if ((*d).owner == NULL)
-printf("nil\n");
-else
-printf("Owner: %s\n", d->owner);
+	if ((*d).age < 0)
+		printf("Age: nil\n");
+	else
+		printf("Age: %f\n", d->age);
+
+
+	if ((*d).owner == NULL)
+		printf("Owner: nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
 
 }
