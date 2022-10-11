cb9b0c493e77f459fc595a701b6fca48d87e346a
tsohleDev <mokhemisitsohle@gmail.com>
2022-10-10 20:39:40 -0700

    refactor
    remove the copying of memory that causses unmessary duplicates in the RAM, and istead use the memory at the arguments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -48,33 +48,19 @@ return (dest);
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
+	dog_t *doggo;
 
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-doggo = malloc(sizeof(dog_t));
-if (doggo == NULL)
-return (NULL);
+	doggo = malloc(sizeof(new_dog));
+	
+	if (doggo == NULL)
+		return (NULL);
 
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
-{
-free(doggo);
-return (NULL);
-}
+	doggo->name = name;
+	doggo->age = age;
+	doggo->owner = owner;
 
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (doggo->owner == NULL)
-{
-free(doggo->name);
-free(doggo);
-return (NULL);
-}
-
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
-
-return (doggo);
+	return (doggo);
 }
