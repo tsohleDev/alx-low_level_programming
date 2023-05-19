17bd9ff155d013a86da2ac8827fc759a9e33abf6
tsohleDev <mokhemisitsohle@gmail.com>
2023-05-19 05:15:22 +0200

    Fix betty lints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- * 
- * Return: Always 0 (Success)
+* main - Entry point
+* 
+* Return: Always 0 (Success)
 */
 
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- * 
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
