c92e10cee9aa1e63c75a54496bebf056fbd540be
tsohleDev <mokhemisitsohle@gmail.com>
2023-05-19 05:04:21 +0200

    Submit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void) {
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, 59);
+    return 1;
+}
