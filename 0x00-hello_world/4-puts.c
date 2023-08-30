d9392afc489503598145c902bbf37e27b9a4fbbc
tsohleDev <mokhemisitsohle@gmail.com>
2023-08-30 04:17:24 +0200

    Add 4,5 Update 1,2,3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,12 @@
 
 /**
 * main - Entry point
-<<<<<<< HEAD
-* 
-=======
 *
->>>>>>> 79bd02eab35ca68637fac884b01ca833d93cc76e
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
